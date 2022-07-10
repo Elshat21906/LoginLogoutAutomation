f71f12ca736eee78a0066ddc4e9269e1fd56bf94
Elshat <z90602190@hotmail.com>
2022-07-10 17:45:40 +0300

    LoginLogoutAutomation

---
+++ src/test/java/com/LoginLogoutAutomation/Pages/LoginPage.java
@@ -0,0 +1,24 @@
+package com.LoginLogoutAutomation.Pages;
+
+import com.LoginLogoutAutomation.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+    @FindBy(id = "login")
+    public WebElement emailField;
+
+    @FindBy(id = "password")
+    public WebElement passwordField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement loginButton;
+}
---
+++ src/test/java/com/LoginLogoutAutomation/Step_Definitions/ValidCredentials.java
@@ -0,0 +1,10 @@
+package com.LoginLogoutAutomation.Step_Definitions;
+
+import com.LoginLogoutAutomation.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class ValidCredentials {
+
+
+
+}
