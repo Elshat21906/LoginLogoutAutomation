ab8cd9037b817131773b7571d73428185382ef20
Elshat <z90602190@hotmail.com>
2022-07-10 22:37:56 +0300

    LoginLogoutAutomation

---
+++ src/test/java/com/LoginLogoutAutomation/utilities/BrowserUtilities.java
@@ -0,0 +1,13 @@
+package com.LoginLogoutAutomation.utilities;
+
+public class BrowserUtilities {
+
+    public static void waitFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
